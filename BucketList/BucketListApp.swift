61a8caa4e59a5c1a091885db7cadbea7a8f92f8e
bvo24 <brianvo2020@gmail.com>
2024-05-16 13:13:00 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Brian Vo on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
