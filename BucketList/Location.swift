322d17cc5939a930e995adae801fd2a1623330c4
bvo24 <brianvo2020@gmail.com>
2024-05-17 12:30:57 -0400

    Day 72

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import MapKit
+
+struct Location : Codable, Equatable, Identifiable{
+    var id: UUID
+    var name: String
+    var description : String
+    var latitude : Double
+    var longitude : Double
+    
+    
+    var coordinate : CLLocationCoordinate2D{
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    static func ==(lhs: Location, rhs: Location) -> Bool{
+        
+        lhs.id == rhs.id
+        
+    }
+    
+    #if DEBUG
+    static let example = Location(id: UUID(), name: "Tysons Corner", description: "Only place at nova", latitude: 38.917130, longitude: -77.222237)
+    #endif
+    
+}
